747172fd5138573b09de5e8c56557cdba848d60e
CARETTE Romain <rrcarette@gmail.com>
2017-05-14 10:45:23 +0200

    lem

--- lem-in/src/ft_getdata.c
+++ lem-in/src/ft_getdata.c
@@ -6,14 +6,14 @@
 /*   By: rcarette <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 00:58:25 by rcarette          #+#    #+#             */
-/*   Updated: 2017/05/13 19:58:37 by rcarette         ###   ########.fr       */
+/*   Updated: 2017/05/14 01:36:59 by rcarette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/data.h"
 #include "../inc/main.h"
 
-static int		if_a_overflow_coord(char *s1, char *s2)
+/*static int		if_a_overflow_coord(char *s1, char *s2)
 {
 	long long	data_1;
 	long long	data_2;
@@ -52,69 +52,43 @@ static int		if_a_connect(char *str)
 	if (ft_strlen_matrice(board = ft_strsplit(str, '-')) != 2)
 		return (clear_tab(board));
 	return (CONNECT);
-}
+}*/
 
-static void		ft_getnbr_fourmi(t_lemin *lemin)
+
+static void		getnbr_fumy(t_lemin *lemin)
 {
 	t_line		*temp_line;
+	char		*copy_line;
 	long long	data;
 
 	temp_line = lemin->line_copy;
 	while (ft_strlen(temp_line->line) == 0)
 		temp_line = temp_line->next;
-	if (!is_digit(temp_line->line))
+	copy_line = ft_strdup(temp_line->line);
+	if ((copy_line[0] == '-' || ft_strlen(copy_line) > 10))
+		temp_line->value = INVALID;
+	else if (!(is_digit(copy_line)))
 	{
 		temp_line->value = INVALID;
 		return ;
 	}
-	if (ft_strlen(temp_line->line) > 11)
+	data = ft_atoi(copy_line);
+	if (data <= 0 || data > MAX_INT)
 		temp_line->value = INVALID;
 	else
-	{
-		data = ft_atoi(temp_line->line);
-		if (data < 0 || data > MAX_INT)
-			temp_line->value = INVALID;
-		else
-			temp_line->value = NBR_FOURMI;
-	}
+		temp_line->value = NBR_FOURMI;
+	ft_memdel((void *)&copy_line);
 }
 
 static void		lexer(t_lemin *lemin)
 {
 	t_line		*temp_line;
-	char		**board;
+	//char		**board;
 
-	ft_getnbr_fourmi(lemin);
+	getnbr_fumy(lemin);
 	temp_line = lemin->line_copy;
 	while (temp_line)
 	{
-		if (temp_line->value == NBR_FOURMI)
-		{
-			temp_line = temp_line->next;
-			continue ;
-		}
-		if (!ft_strlen(temp_line->line))
-			temp_line->value = INVALID;
-		else if (temp_line->line[0] == '#')
-		{
-			if (!ft_strcmp(temp_line->line, "##start"))
-				temp_line->value = START;
-			else if (!ft_strcmp(temp_line->line, "##end"))
-				temp_line->value = END;
-			else
-				temp_line->value = COMMENTAIRE;
-		}
-		else
-		{
-			board = ft_strsplit(temp_line->line, ' ');
-			if (ft_strlen_matrice(board) == 3)
-				temp_line->value = if_a_room(board[1], board[2]);
-			else if (if_a_connect(temp_line->line))
-				temp_line->value = CONNECT;
-			else
-				temp_line->value = INVALID;
-			clear_tab(board);
-		}
 		temp_line = temp_line->next;
 	}
 }
